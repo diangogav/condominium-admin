e98fd358715849cbc97eafc2c02303c7e8f1697f
DiangoGav <diango.code@gmail.com>
2026-02-01 12:05:00 -0400

    Revert "fix: update API_BASE_URL to use proxy and add rewrites configuration"
    This reverts commit 258e4aa3576cf204eab87296e899f3ed1d5b410e.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/proxy/:path*',
-        destination: 'http://condominio.api.diangogavidia.com:3000/:path*',
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
